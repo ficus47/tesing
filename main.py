8a6f14b98cf6da2fcd347b0c39a6397eebe694aa
ficus47 <140836421+ficus47@users.noreply.github.com>
2024-06-29 17:08:18 +0000

    h

--- main.py
+++ main.py
@@ -18,16 +18,16 @@ GA_AdSense = """
       </script>
     """
 
-    # Insert the script in the head tag of the static template inside your virtual
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    st.write(f'editing {index_path}')
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(script, src=adsense_url): 
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  
-        else:
-            shutil.copy(index_path, bck_index)  
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_AdSense)
-        index_path.write_text(new_html)
+# Insert the script in the head tag of the static template inside your virtual
+index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+st.write(f'editing {index_path}')
+soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+if not soup.find(script, src=adsense_url): 
+    bck_index = index_path.with_suffix('.bck')
+    if bck_index.exists():
+        shutil.copy(bck_index, index_path)  
+    else:
+        shutil.copy(index_path, bck_index)  
+    html = str(soup)
+    new_html = html.replace('<head>', '<head>\n' + GA_AdSense)
+    index_path.write_text(new_html)
